6102049aa1a0eb2ca5f6331fb1eb79a9aeaf97b4
Yared3214 <yaredman0099@gmail.com>
2025-02-07 19:14:47 -0800

    added glowing effect and added navigation links

--- app/page.js
+++ app/page.js
@@ -1,13 +1,61 @@
+"use client"
 import Image from "next/image";
-import reactLogo from "./assets/react-logo.webp"
+import reactLogo from "./assets/react-logo.png"
 import nextLogo from "./assets/next.svg"
 import expressLogo from "./assets/express-logo.png"
 import nodeLogo from "./assets/node-logo.png"
 import mongoLogo from "./assets/mongo-logo.png"
 import graphqlLogo from "./assets/graphql-logo.png"
 import hygraphLogo from "./assets/hygraph-logo.jpg"
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+const sections = ["about", "skills", "project"];
 
 export default function Home() {
+
+  const [activeSection, setActiveSection] = useState(sections[0]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY + window.innerHeight / 2; // Middle of the viewport
+      let currentSection = sections[0];
+
+      for (const section of sections) {
+        const element = document.getElementById(section);
+        if (element) {
+          const { top, bottom } = element.getBoundingClientRect();
+          const offsetTop = top + window.scrollY;
+          const offsetBottom = bottom + window.scrollY;
+
+          if (scrollPosition >= offsetTop && scrollPosition < offsetBottom) {
+            currentSection = section;
+            break;
+          }
+        }
+      }
+
+      setActiveSection(currentSection);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    const cursorGlow = document.getElementById("cursor-glow");
+
+    const moveCursor = (e) => {
+      if (cursorGlow) {
+        cursorGlow.style.transform = `translate(${e.clientX - 80}px, ${e.clientY - 80}px)`;
+      }
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+    return () => window.removeEventListener("mousemove", moveCursor);
+  }, []);
+
+
   const frontSkills = [
     {
       title: "React js",
@@ -71,75 +119,104 @@ export default function Home() {
     }
   ]
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24 px-36">
+    <main className="flex min-h-screen flex-col items-center justify-between px-36">
+      {/* Glow Effect */}
+      <div id="cursor-glow" className="pointer-events-none fixed top-0 left-0 h-[500px] w-[500px] rounded-full bg-blue-200 opacity-5 blur-3xl mix-blend-color-dodge transition-transform duration-100"></div>
       <div className="grid grid-cols-3">
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 sticky top-0 h-screen overflow-y-auto py-24">
           <div className="text-5xl text-white">Yared Bitewlign</div>
           <div className="text-xl text-white">Web And App developer</div>
           {/* nav bar  */}
-          <nav className="mt-24">
+          {/* <nav className="mt-24">
             <ul className="flex flex-col gap-5">
               <li>
-                <a href="#about" className="group flex items-center">
-                  <span class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 
+                <Link href="#about" className="group flex items-center">
+                  <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 
                 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 
                 motion-reduce:transition-none"></span>
-                  <span class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 
+                  <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 
                 group-hover:text-slate-200 group-focus-visible:text-slate-200">About</span>
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#skills" className="group flex items-center">
-                  <span class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 
+                <Link href="#skills" className="group flex items-center">
+                  <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 
                 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 
                 motion-reduce:transition-none"></span>
-                  <span class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 
+                  <span data-testid="skills-section" className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 
                 group-hover:text-slate-200 group-focus-visible:text-slate-200">Skills</span>
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#projects" className="group flex items-center">
-                  <span class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 
+                <Link href="#project" className="group flex items-center">
+                  <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 
                 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 
                 motion-reduce:transition-none"></span>
-                  <span class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 
+                  <span data-testid="project-section" className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 
                 group-hover:text-slate-200 group-focus-visible:text-slate-200">Projects</span>
-                </a>
+                </Link>
               </li>
+            </ul>
+          </nav> */}
+          <nav className="mt-24">
+            <ul className="flex flex-col gap-5">
+              {sections.map((section) => (
+                <li key={section}>
+                  <Link href={`#${section}`} className="group flex items-center">
+                    <span
+                      className={`nav-indicator mr-4 h-px transition-all ${activeSection === section
+                        ? "w-16 bg-slate-200"
+                        : "w-8 bg-slate-600"
+                        }`}
+                    ></span>
+                    <span
+                      className={`nav-text text-xs font-bold uppercase tracking-widest transition-colors ${activeSection === section
+                        ? "text-slate-200"
+                        : "text-slate-500"
+                        }`}
+                    >
+                      {section.charAt(0).toUpperCase() + section.slice(1)}
+                    </span>
+                  </Link>
+                </li>
+              ))}
             </ul>
           </nav>
         </div>
         {/* Content side of the page */}
-        <div className="col-span-2">
+        <div className="col-span-2 py-24">
           {/* self intro part */}
-          <p id="about">I am a computer science student at Addis Ababa University, specializing in web development as a
-            frontend designer and developer. I have worked on e-learning, e-commerce, and other projects, and
-            am currently developing an AI-featured fundraising platform named "Bright-Fund." My career goals
-            include working in a tech company to participate in impactful projects and gain valuable experience.
-            Outside of my studies, I enjoy watching movies and reading psychology and philosophy books. I am
-            motivated by the desire to address and solve the numerous problems in my country, aiming to make a
-            significant impact in the future.</p>
+          <section id="about" className="min-h-[50vh]">
+            <p>I am a computer science student at Addis Ababa University, specializing in web development as a
+              frontend designer and developer. I have worked on e-learning, e-commerce, and other projects, and
+              am currently developing an AI-featured fundraising platform named "Bright-Fund." My career goals
+              include working in a tech company to participate in impactful projects and gain valuable experience.
+              Outside of my studies, I enjoy watching movies and reading psychology and philosophy books. I am
+              motivated by the desire to address and solve the numerous problems in my country, aiming to make a
+              significant impact in the future.</p>
+          </section>
+
           {/* My Skills part of the page */}
-          <div className="mt-32">
-            {/* Frontend Skils */}
-            <div>
+          <div className="">
+            <section id="skills" className="min-h-[50vh]">
+              {/* Frontend Skils */}
               <div className="text-3xl text-white mb-5">Frontend</div>
               {frontSkills.map((skill, index) => (
-                <div id={index} className="group relative grid pb-1 transition-all sm:grid-cols-4 sm:gap-8 md:gap-4 lg:hover:!opacity-100 
+                <div key={index} className="group relative grid pb-1 transition-all sm:grid-cols-4 sm:gap-8 md:gap-4 lg:hover:!opacity-100 
                 lg:group-hover/list:opacity-50 mb-7">
-                  <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none 
+                  <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none 
                   lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] 
                   lg:group-hover:drop-shadow-lg"></div>
                   <div className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-1">
-                    <Image src={skill.logo} width={1200} height={300}
+                    <Image alt='icon-image' src={skill.logo} width={1200} height={300}
                       className="w-3/4" />
                   </div>
                   <div className="col-span-3">
                     <div className="font-medium leading-snug text-slate-200">{skill.title}</div>
                     <p className="mt-2 text-sm leading-normal">{skill.desc}</p>
-                    <ul id={index} className="mt-2 flex flex-wrap" aria-label="Technologies Used Together">
+                    <ul key={index} className="mt-2 flex flex-wrap" aria-label="Technologies Used Together">
                       {skill.otherTech.map((other, index) => (
-                        <ul>
+                        <ul key={index}>
                           <li className="mr-1.5 mt-2">
                             <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5
                         text-teal-300 ">{other}</div>
@@ -150,17 +227,15 @@ export default function Home() {
                   </div>
                 </div>
               ))}
-            </div>
-            <div>
               <div className="text-3xl text-white mt-24  mb-7">Backend</div>
               {backSkills.map((skill, index) => (
-                <div id={index} className="group relative grid pb-1 transition-all sm:grid-cols-4 sm:gap-8 md:gap-4 lg:hover:!opacity-100 
+                <div key={index} className="group relative grid pb-1 transition-all sm:grid-cols-4 sm:gap-8 md:gap-4 lg:hover:!opacity-100 
                 lg:group-hover/list:opacity-50 mb-7">
-                  <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none 
+                  <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none 
                   lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] 
                   lg:group-hover:drop-shadow-lg"></div>
                   <div className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-1">
-                    <Image src={skill.logo} width={1200} height={300}
+                    <Image alt={skill.title} src={skill.logo} width={1200} height={300}
                       className="w-3/4" />
                   </div>
                   <div className="col-span-3">
@@ -179,41 +254,41 @@ export default function Home() {
                   </div>
                 </div>
               ))}
-            </div>
-            <div>
-              <div className="text-3xl text-white mt-24">Other Skills</div>
-              <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
-                {/* <!-- Skill Item: Problem-Solving & Debugging --> */}
-                <div class="flex flex-col items-center">
-                  <img src="https://img.icons8.com/color/48/000000/bug.png" alt="Problem-Solving & Debugging Icon" class="w-16 h-16" />
-                  <p class="mt-2 text-center text-lg font-semibold">Problem-Solving & Debugging</p>
-                </div>
+              <div>
+                <div className="text-3xl text-white mt-24">Other Skills</div>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+                  {/* <!-- Skill Item: Problem-Solving & Debugging --> */}
+                  <div className="flex flex-col items-center">
+                    <img src="https://i.ibb.co/PZM6bqB/debugging.png" alt="Problem-Solving & Debugging Icon" class="w-16 h-16" />
+                    <p className="mt-2 text-center text-lg font-semibold">Problem-Solving & Debugging</p>
+                  </div>
+                  {/* <!-- Skill Item: Responsive Design --> */}
+                  <div className="flex flex-col items-center">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2535/2535547.png" alt="Responsive Design Icon" class="w-16 h-16" />
+                    <p className="mt-2 text-center text-lg font-semibold">Responsive Design</p>
+                  </div>
 
-                {/* <!-- Skill Item: Responsive Design --> */}
-                <div class="flex flex-col items-center">
-                  <img src="https://cdn-icons-png.flaticon.com/512/2535/2535547.png" alt="Responsive Design Icon" class="w-16 h-16" />
-                  <p class="mt-2 text-center text-lg font-semibold">Responsive Design</p>
-                </div>
+                  {/* <!-- Skill Item: Design Tools (Figma) --> */}
+                  <div className="flex flex-col items-center">
+                    <img src="https://img.icons8.com/color/48/000000/figma.png" alt="Figma Icon" NameclassName="w-16 h-16" />
+                    <p className="mt-2 text-center text-lg font-semibold">Design Tools (Figma)</p>
+                  </div>
 
-                {/* <!-- Skill Item: Design Tools (Figma) --> */}
-                <div class="flex flex-col items-center">
-                  <img src="https://img.icons8.com/color/48/000000/figma.png" alt="Figma Icon" class="w-16 h-16" />
-                  <p class="mt-2 text-center text-lg font-semibold">Design Tools (Figma)</p>
-                </div>
+                  {/* <!-- Skill Item: Collaboration & Communication --> */}
+                  <div className="flex flex-col items-center">
+                    <img src="https://static.vecteezy.com/system/resources/thumbnails/000/534/837/small/gdpr_line_solid-13.jpg" alt="Collaboration & Communication Icon" class="w-16 h-16" />
+                    <p className="mt-2 text-center text-lg font-semibold">Collaboration & Communication</p>
+                  </div>
 
-                {/* <!-- Skill Item: Collaboration & Communication --> */}
-                <div class="flex flex-col items-center">
-                  <img src="https://static.vecteezy.com/system/resources/thumbnails/000/534/837/small/gdpr_line_solid-13.jpg" alt="Collaboration & Communication Icon" class="w-16 h-16" />
-                  <p class="mt-2 text-center text-lg font-semibold">Collaboration & Communication</p>
-                </div>
-
-                {/* <!-- Skill Item: Version Control --> */}
-                <div class="flex flex-col items-center">
-                  <img src="https://img.icons8.com/color/48/000000/git.png" alt="Version Control Icon" class="w-16 h-16" />
-                  <p class="mt-2 text-center text-lg font-semibold">Version Control</p>
+                  {/* <!-- Skill Item: Version Control --> */}
+                  <div className="flex flex-col items-center">
+                    <img src="https://img.icons8.com/color/48/000000/git.png" alt="Version Control Icon" class="w-16 h-16" />
+                    <p className="mt-2 text-center text-lg font-semibold">Version Control</p>
+                  </div>
                 </div>
               </div>
-
+            </section>
+            <div className="mt-32">
             </div>
           </div>
         </div>
